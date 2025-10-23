7af80cd9bd48e61a315551873f7cf8890541cdac
victor-sc12 <victorsuquilanda8639@outlook.com>
2025-10-23 13:30:10 -0500

    Configuración de archivos estáticos locales (de app) y globales.

--- playGround/quotes/views.py
+++ playGround/quotes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
 from django.urls import reverse
 
 # Dict to quotes:
@@ -30,7 +30,13 @@ def messages_per_day(request, day): # Imporante que el name del parametro enviad
     try:
         return HttpResponse(quotes[day])
     except KeyError:
-        return HttpResponseNotFound('dia no encontrado')
+        # En teoría si se acciona este except es porqué tenemos un '404' code. Pero al aplicar render estaremos
+        # mostrando un template que se carga con '200' code. Es decir en realidad no hay '404' code.
+        return render(request, '404.html')
+    
+        # si de verdad deseamos renderizar una plantilla cuando obtengamos un '404', se tendrá que instanciar 'Http404'.
+        # Pero esta opción solo esta disponible cuando el proyecto este desplegado (es decir cuando DEBUG=False)
+        # return Http404() --> detecta un template con nombre '404.html' automaticamente.
     
 def messages_per_day_with_number(request, day):
     days_keys_list = list(quotes.keys())
