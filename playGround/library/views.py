eb0f6dbf1b0c9fa312f5893e55c813eb1011e2a7
victor-sc12 <victorsuquilanda8639@outlook.com>
2025-11-16 17:32:15 -0500

    Incorporación de paginador y nuevos filtros por fecha, título y autor en vista de app 'library'.

--- playGround/library/views.py
+++ playGround/library/views.py
@@ -1,26 +1,40 @@
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q
 from django.http import HttpResponseNotFound
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     try:
         libros = Libro.objects.all()
-        autor_id = request.GET.get('autor')
-        genero_id = request.GET.get('genero')
+        query_filter = request.GET.get('query_search')
+        date_start = request.GET.get('start')
+        date_end = request.GET.get('end')
 
-        if autor_id and genero_id:
-            libros = libros.filter(autor__id = autor_id)
-            autor = Autor.objects.get(id = autor_id)
+        # capturar título o nombre de autor
+        if query_filter:
+            libros = libros.filter(Q(title__icontains = query_filter) | Q(autor__name__icontains = query_filter))
+        
+        # capturar fechas
+        if date_start and date_end:
+            libros = libros.filter(publication_date__range=[date_start, date_end])
 
-        if genero_id:
-            libros = libros.filter(generos__id = genero_id)
-            genero = Genero.objects.get(id=genero_id)
+        # Paginator (el paginador siempre debe ir al final de todos los filters)
+        paginator = Paginator(libros, 4)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        # capturar url y sus parametros:
+        query_params = request.GET.copy()
+        if 'page' in query_params:
+            query_params.pop('page')
+        query_string = query_params.urlencode()
 
         return render(request, 'library/index.html', {
-            'libros': libros,
-            'autor': autor,
-            'genero': genero,
+            'libros': page_obj,
+            'query': query_filter,
+            'query_string': query_string,
         })
     except Exception:
         return HttpResponseNotFound('Page not found')
